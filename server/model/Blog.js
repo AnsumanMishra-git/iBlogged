c10e954a0bcec3c2838489b00ddcb139156cda4c
Ansuman Mishra <56594734+AnsumanMishra-git@users.noreply.github.com>
2022-06-12 01:11:37 +0530

    Add files via upload

---
+++ server/model/Blog.js
@@ -0,0 +1,27 @@
+// creating the Blog collection inside our DB
+
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+export default mongoose.model("Blog", blogSchema);
